e62d21f385f30c6d6d15efb1a7e781248a84a5ac
Eldar Yusupov <eyusupov@gmail.com>
2024-05-11 14:48:05 +0300

    Add more tests with blank nodes

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,7 @@
-from rdflib import BNode, Literal, Namespace, Variable
+from rdflib import RDF, BNode, Graph, Literal, Namespace, URIRef, Variable
+from rdflib.collection import Collection
+
+from knom import LOG
 
 EX = Namespace("http://example.com/")
 var_a = Variable("a")
@@ -10,3 +13,37 @@ lit_c = Literal("c")
 bn_a = BNode("a")
 bn_b = BNode("b")
 bn_c = BNode("c")
+
+MF = Namespace("http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#")
+
+
+def generate_tests_from_manifests(path: str, metafunc) -> None:  # noqa: ANN001
+    g = Graph().parse(location=path, format="n3")
+    names = []
+    parameters = []
+    for manifest, _, _ in g.triples((None, RDF.type, MF.Manifest)):
+        mf_name = g.value(manifest, MF.name, None)
+        entries = g.value(manifest, MF.entries, None)
+        assert entries is not None, "could not find entries in the test manifest"
+        for entry in Collection(g, entries):
+            name = g.value(entry, MF.name, None)
+            action = g.value(entry, MF.action, None)
+            result = g.value(entry, MF.result, None)
+            assert action is not None, f"action is empty for {name}"
+            assert result is not None, f"result is empty for {name}"
+            assert isinstance(action, URIRef)
+            assert isinstance(result, URIRef)
+            names.append(f"{mf_name}__{name}".replace(" ", "_"))
+            parameters.append((action, result))
+        metafunc.parametrize("action, result", parameters, ids=names)
+
+
+def split_rules_and_facts(graph: Graph) -> tuple[Graph, Graph]:
+    rules = Graph(namespace_manager=graph.namespace_manager)
+    facts = Graph(namespace_manager=graph.namespace_manager)
+    for s, p, o in graph:
+        if p == LOG.implies:
+            rules.add((s, p, o))
+        else:
+            facts.add((s, p, o))
+    return (rules, facts)
--- tests/test_naive_fixpoint.py
+++ tests/test_naive_fixpoint.py
@@ -1,23 +1,22 @@
 from rdflib import Graph, URIRef
 from rdflib_canon import CanonicalizedGraph
 
-from knom import single_pass
+from knom import naive_fixpoint
 
 from . import generate_tests_from_manifests, split_rules_and_facts
 
 
 def pytest_generate_tests(metafunc) -> None:  # noqa: ANN001
-    generate_tests_from_manifests("tests/n3/single-pass-manifests.n3", metafunc)
+    generate_tests_from_manifests(
+        "tests/n3/positive-non-recursive-rules-manifests.n3", metafunc
+    )
 
 
 def test_single_pass(action: URIRef, result: URIRef) -> None:
     action_graph = Graph().parse(location=action, format="n3")
     rules, facts = split_rules_and_facts(action_graph)
-    id_ = URIRef("#")
-    output = Graph(identifier=id_)
-    for triple in single_pass(facts, rules):
-        output.add(triple)
-    result_graph = Graph(identifier=id_).parse(location=result, format="n3")
+    output = naive_fixpoint(facts, rules)
+    result_graph = Graph().parse(location=result, format="n3")
     assert set(CanonicalizedGraph(output).canon) == set(
         CanonicalizedGraph(result_graph).canon
     )
